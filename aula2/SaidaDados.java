6c9bbc4af4084bfcdde1f0f0ecaca709d656b0cd
takata2903 <84953191+takata2903@users.noreply.github.com>
2024-06-25 15:56:20 +0000

    Testing SaidaDados

---
+++ aula2/SaidaDados.java
@@ -0,0 +1,8 @@
+public class SaidaDados {
+    public static void main(String[] args) {
+        System.out.print("Hello, ");
+        System.out.println("world!");
+        System.out.printf("O valor de PI é %.2f\n", Math.PI);
+    }
+}
+    
